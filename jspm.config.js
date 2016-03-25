166488ea9e8b7ebd9df44a169418ab9d85b77737
Bartosz Gościński <bargosc@gmail.com>
2016-03-25 20:17:31 +0100

    create package.json and init jspm

---
+++ jspm.config.js
@@ -0,0 +1,78 @@
+SystemJS.config({
+  transpiler: "plugin-babel",
+  packages: {
+    "redux-observable": {
+      "main": "redux-observable.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
+  map: {
+    "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
+    "events": "github:jspm/nodelibs-events@0.2.0-alpha",
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.8",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "redux": "npm:redux@3.3.1",
+    "rxjs": "npm:rxjs@5.0.0-beta.3",
+    "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
+    "util": "github:jspm/nodelibs-util@0.2.0-alpha"
+  },
+  packages: {
+    "github:jspm/nodelibs-buffer@0.2.0-alpha": {
+      "map": {
+        "buffer-browserify": "npm:buffer@4.5.1"
+      }
+    },
+    "github:jspm/nodelibs-stream@0.2.0-alpha": {
+      "map": {
+        "stream-browserify": "npm:stream-browserify@2.0.1"
+      }
+    },
+    "npm:buffer@4.5.1": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.2",
+        "ieee754": "npm:ieee754@1.1.6",
+        "isarray": "npm:isarray@1.0.0"
+      }
+    },
+    "npm:loose-envify@1.1.0": {
+      "map": {
+        "js-tokens": "npm:js-tokens@1.0.2"
+      }
+    },
+    "npm:readable-stream@2.0.6": {
+      "map": {
+        "core-util-is": "npm:core-util-is@1.0.2",
+        "inherits": "npm:inherits@2.0.1",
+        "isarray": "npm:isarray@1.0.0",
+        "process-nextick-args": "npm:process-nextick-args@1.0.6",
+        "string_decoder": "npm:string_decoder@0.10.31",
+        "util-deprecate": "npm:util-deprecate@1.0.2"
+      }
+    },
+    "npm:redux@3.3.1": {
+      "map": {
+        "lodash": "npm:lodash@4.6.1",
+        "lodash-es": "npm:lodash-es@4.6.1",
+        "loose-envify": "npm:loose-envify@1.1.0"
+      }
+    },
+    "npm:stream-browserify@2.0.1": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "readable-stream": "npm:readable-stream@2.0.6"
+      }
+    }
+  }
+});
